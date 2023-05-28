cdf48754ca5feeec6eb5ad621345fec334d5800e
HAlGhanim <imzaji98@gmail.com>
2023-05-28 20:54:03 +0300

    2nd task I feel like I overcomplicated it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
-import image from "./assets/images/myPhoto.jpg";
+import gStore from "./assets/images/grocery-store.jpg";
+import prods from "./products";
 function App() {
-  return (
-    <div className="container">
-      <h1>My first React task</h1>
-      <h4>This is an image of myself</h4>
-      <img src={image} className="myImage" alt="me"></img>
+  let productsList = prods.map((prod) => (
+    <div className="fruit1">
+      <img src={prod.image} alt="Mango" className="prodimg"></img>
+      <div>{prod.name}</div>
+      <div>{prod.price}</div>
     </div>
+  ));
+
+  return (
+    <>
+      <div className="container">
+        <h1>Grocery store</h1>
+        <h4>Buy your favorite fruits here</h4>
+        <img src={gStore} className="store" alt="market"></img>
+      </div>
+      <div className="products">
+        <div className="prods">
+          <div className="fruit1">
+            <img src={prods[0].image} alt="Mango" className="prodimg"></img>
+            <div>{prods[0].name}</div>
+            <div>{prods[0].price}</div>
+          </div>
+          <div className="fruit2">
+            <img src={prods[1].image} alt="Apple" className="prodimg"></img>
+            <div>{prods[1].name}</div>
+            <div>{prods[1].price}</div>
+          </div>
+          {productsList}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,22 @@
+import apple from "./assets/images/Apple.jpg";
+import mango from "./assets/images/Mango.jpg";
+
+const prods = [
+  {
+    name: "Mango",
+    price: "800 fils",
+    image: mango,
+  },
+  {
+    name: "Apple",
+    price: "700 fils",
+    image: apple,
+  },
+  {
+    name: "Apple",
+    price: "700 fils",
+    image: apple,
+  },
+];
+
+export default prods;
